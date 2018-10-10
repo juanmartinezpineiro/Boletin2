aeb8ce434d02421ba8d8e2864430197008c29710
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2018-10-10 11:42:31 +0200

    No iba por culpa de no importar la clase Scanner

--- Boletin2_4/src/boletin2_4/Boletin2_4.java
+++ Boletin2_4/src/boletin2_4/Boletin2_4.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package boletin2_4;
-
+import java.util.Scanner;
 /**
  *
  * @author jmartinezpineiro
@@ -17,15 +17,16 @@ public class Boletin2_4 {
     public static void main(String[] args) {
         // TODO code application logic here
     Scanner leer = new Scanner (System.in);
-    int cantidadEntera= 0;
+    int cantidadEntera;
+    int billetes100 , billetes20,billetes5 ,monedas1 ;
     System.out.println("Introduzca la cantidad entera: ");
     cantidadEntera=leer.nextInt();
-    int billetes100=0, billetes20=0,billetes5=0,monedas1=0;
+    
     billetes100= cantidadEntera/100;
     billetes20= cantidadEntera%100/20;
     billetes5= cantidadEntera%100/5;
     monedas1= cantidadEntera%100/1;
-    System.out.println(""+billetes100+""+billetes20+""+billetes5+""+monedas1);
+    System.out.println("Son "+billetes100+" billete(s) de 100: "+billetes20+" billete(s) de 20: "+billetes5+" billete(s) de 5: "+monedas1+ " :moneda(s) de 1");
     
     
     }
